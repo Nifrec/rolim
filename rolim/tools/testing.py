703e849ed679d77d65afd81b21e19928f614d48f
nifrec <lulof.piree@zoho.com>
2022-10-21 09:17:02 +0200

    Add auxiliary function for testing tensor equality

---
+++ rolim/tools/testing.py
@@ -0,0 +1,43 @@
+"""
+© Lulof Pirée 2022
+
+--------------------------------------------------------------------------------
+Part of "rolim": Reproduction Of the paper "Latent World Models
+For Intrinsically Motivated Exploration"
+by Alexander Ermolov and Nice Sebe (2020).
+The reproduction is performed by Lulof Pirée
+as part of the course 'Seminar Advanced Deep Reinforcement Learning'
+at Leiden University.
+
+Author: Lulof Pirée
+Fall 2022
+
+Copyright (C) 2022 Lulof Pirée
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+--------------------------------------------------------------------------------
+*File content:*
+
+Helper functions for writing testcases.
+"""
+# Library imports:
+import torch
+from torch import Tensor
+
+def assert_tensor_eq(expected: Tensor, result: Tensor):
+    assert (expected.shape == result.shape), \
+            f"Shape mismatch; expected: {expected.shape}, got: {result.shape}"
+    assert torch.allclose(expected, result), \
+            f"Elements mismatch. expected:\n{expected}\nGot:\n{result}"
