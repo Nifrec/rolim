31af1e9e02a869fc7bc16ad22fa9ea771f77ab9f
nifrec <lulof.piree@zoho.com>
2022-10-21 09:00:08 +0200

    Add template source-code file

---
+++ rolim/template.py
@@ -0,0 +1,42 @@
+"""
+© Lulof Pirée 2022
+
+--------------------------------------------------------------------------------
+Part of "rolim": Reproduction Of the paper "Latent World Models
+For Intrinsically Motivated Exploration"
+by Alexander Ermolov and Nice Sebe (2020).
+The reproduction is performed by Lulof Pirée
+as part of the course 'Seminar Advanced Deep Reinforcement Learning'
+at Leiden University.
+
+Author: Lulof Pirée
+Fall 2022
+
+Copyright (C) 2022 Lulof Pirée
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+--------------------------------------------------------------------------------
+*File content:*
+
+This is a template file for new source-code files
+in this project. 
+TODO: substitute this placeholder with actual description.
+"""
+# Library imports:
+import torch
+from torch import Tensor
+
+# Local imports:
+
