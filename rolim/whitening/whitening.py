bd51efbcabcb51367097aee3ca826b87c92b9551
nifrec <lulof.piree@zoho.com>
2022-10-22 21:54:37 +0200

    Make first attempt at improved whitening transform

--- rolim/whitening/whitening.py
+++ rolim/whitening/whitening.py
@@ -45,9 +45,70 @@ from torch import Tensor
 # Local imports:
 from rolim.tools.stats import sample_covar
 from rolim.tools.testing import assert_tensor_eq
+from rolim.settings import WHITEN_REG_EPS
 
-def whiten(vectors: Tensor) -> Tensor:
+def whiten(vectors: Tensor, 
+           reg_eps: float = WHITEN_REG_EPS,
+           do_validate: bool=False) -> Tensor:
     """
+    Given a matrix `vectors` of column-vectors,
+    subtract the mean of the vectors from each vector,
+    and 'divide by the standard deviation'
+    (left-multiply each mean-subtracted vector with the matrix W,
+    which has the property that W.t@W = Σ^{-1},
+    the inverse of the sample covariance of `vectors`.
+
+    The implementation details are given in the appendix of
+    Aliaksandr Siarohin, Enver Sangineto & Nicu Sebe (2019)
+    'Whitening and Colouring batch transform for GANs'.
+
+    Arguments:
+    * vectors: matrix of column-vectors to apply the
+        whitening transform to.
+        Must be of floating datatype.
+    * reg_eps: small positive constant to add to the
+        diagonal entries of the covariance matrix of `vectors`
+        to make it better conditioned for inversion
+        (a needed step to compute the whitening transform).
+    * do_validate: flag whether to run assertions
+        on the correctness of decompositions and versions.
+
+    Returns:
+    * Copy of `vectors`, with the whitening transform applied
+        to each column-vector.
+    """
+    mean = torch.mean(vectors, dim=1)
+    raw_covar = sample_covar(vectors, ddof=1)
+    identity = torch.eye(raw_covar.shape[0])
+    reg_covar = (identity - reg_eps) @ raw_covar + reg_eps*identity
+    # The cited paper *first* runs the decomposition,
+    # and thereafter inverts the decomposition-output.
+    # The main text in papers make it look like done the other way around.
+    w = torch.linalg.cholesky(reg_covar)
+
+    if do_validate:
+        _validate_whiten(w, vectors)
+
+    # The output of the Cholesky transform is always lower-triangular
+    return torch.triangular_solve(w, vectors - mean, upper=False)
+
+def _validate_whiten(w: Tensor, vectors: Tensor):
+    # Check if the decomposition is correct
+    assert_tensor_eq(w @ w.T, vectors)
+    # Check if w is indeed invertible
+    identity = torch.eye(w.shape[0])
+    w_inv = torch.triangular_solve(w, identity,
+                                   upper=False),
+    assert_tensor_eq(w_inv @ w, identity)
+
+def whiten_naive(vectors: Tensor) -> Tensor:
+    """
+    WARNING: this is the naive implementation
+    of the whitening transorm, following the
+    description in the main text of papers.
+    It generally fails to properly invert a covariance matrix.
+    Use the function `whiten` for a better alternative.
+
     Given a matrix `vectors` of column-vectors,
     subtract the mean of the vectors from each vector,
     and 'divide by the standard deviation'
@@ -63,7 +124,6 @@ def whiten(vectors: Tensor) -> Tensor:
         raise RuntimeError("Unable to invert the matrix:\n"+str(covar)
                            + "\n Resulting product Σ^{-1} @ Σ:\n"
                            + str(covar_inv @ covar))
-    # assert_tensor_eq(identity, covar_inv @ covar)
     w = torch.linalg.cholesky(covar_inv)
     return w @ (vectors - mean)
     
