9f97e9917edb83a215bf66105f418b02fb9d82e3
nifrec <lulof.piree@zoho.com>
2022-11-25 15:08:27 +0100

    Cleanup broken worker-spawning-code in multiple_runs.py

--- rolim/encoder/multiple_runs.py
+++ rolim/encoder/multiple_runs.py
@@ -78,8 +78,7 @@ T = TypeVar("T")
 
 WorkerJob = namedtuple("WorkerJob",
                        ("loss_fun", "num_batches", "batch_size",
-                        "test_set_batch", "save_dir", "run_num",
-                        "jobs_per_worker")
+                        "test_set_batch", "save_dir", "run_num")
                        )
 
 class SubdirNames(enum.Enum):
@@ -197,13 +196,8 @@ def train_enc_multiple_runs(
                       batch_size = batch_size,
                       test_set_batch = test_set_batch,
                       save_dir = save_dir,
-                      run_num = num,
-                      jobs_per_worker=num_runs//num_workers)
+                      run_num = num)
             for num in range(num_runs)]
-    # procs = [multiprocessing.spawn(__perform_run, args=(job,), nprocs=1,
-                              # join=False) for job in jobs]
-    # for proc in procs:
-        # proc.join()
     __dispatch_jobs(jobs, num_workers)
     return save_dir
 
@@ -219,23 +213,6 @@ def __dispatch_jobs(jobs: list[WorkerJob], num_workers: int):
         print(f"Current batch of {len(next_jobs)} jobs all finished.")
     print("All jobs (experiment runs) have finished.")
 
-def __run_batch_of_jobs(worker_idx: int, jobs: list[WorkerJob]):
-    num_jobs_per_worker = jobs[0].jobs_per_worker
-    num_jobs = len(jobs)
-    my_job_indices = list(range(worker_idx * num_jobs_per_worker,
-                                max((worker_idx+1) * num_jobs_per_worker,
-                                    num_jobs)))
-    print(f"Starting jobs {my_job_indices} in parallel"
-          f" out of {num_jobs} jobs.")
-    my_jobs = [jobs[idx] for idx in my_job_indices]
-    procs = [multiprocessing.spawn(__perform_run, args=(job,), nprocs=1,
-                              join=False) for job in my_jobs]
-    for proc in procs:
-        proc.join()
-    print(f"Finished jobs {my_job_indices}.")
-
-def __worker_setup():
-    torch.set_default_tensor_type(torch.cuda.FloatTensor)
 
 def __save_parameters(num_runs: int,
                       loss_fun: Literal["MSE", "W-MSE", "DW-MSE"],
